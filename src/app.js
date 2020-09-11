92638ab763ce20870cde5862928eeabe870de4fc
Marlena Nikosey <mknikosey@yahoo.com>
2020-09-10 21:44:19 -0400

    Added IDs to elements

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
+function displayTemperature () {
+
+}
+
+let apiKey = "613c0e173ab2e750c5de3e90ad00ac92";
+let apiUnits = "imperial";
+let city = "Boston";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+
+axios.get(apiUrl).then(displayTemperature);
+
+
+console.log(apiUrl);
+
+
+
+
 
