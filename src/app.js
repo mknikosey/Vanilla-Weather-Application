9265df2a49e0ff580c9875b5d6b816f3a91e68c2
Marlena Nikosey <mknikosey@yahoo.com>
2020-09-12 18:22:06 -0400

    Updated forecast to be 3+ hourly

--- src/app.js
+++ src/app.js
@@ -22,6 +22,7 @@ function formatDate (timestamp) {
 
 
 
+
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
@@ -46,13 +47,41 @@ function displayTemperature(response) {
 }
 
 
+function displayForecast(response) {
+    let forecastElement = document.querySelector("#forecast");
+    forecastElement.innerHTML = null;
+    let forecast = null;
+
+    for (let index = 0; index <6; index++) {
+    forecast = response.data.list[index];
+    forecastElement.innerHTML += `
+    <div class="col-2">
+         <h3>
+             ${formatHours(forecast.dt * 1000)}
+        </h3>
+         <img 
+         src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png"/>
+        <div class="weather-forecast-temperature">
+        <strong>
+        ${Math.round(forecast.main.temp_max)}°
+        </strong> 
+        ${Math.round(forecast.main.temp_min)}°
+        </div>
+    </div>
+    `; }
+}
+
+
 function search(city) {
     let apiKey = "613c0e173ab2e750c5de3e90ad00ac92";
     let units = "imperial";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
     axios.get(apiUrl).then(displayTemperature);
+    
+    apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=${units}`;
+    axios.get(apiUrl).then(displayForecast); }
 
-}
+
 
 function handleSubmit(event) {
     event.preventDefault();
@@ -92,11 +121,3 @@ celciusLink.addEventListener("click", displayCelciusTemperature);
 
 
 search("Boston");
-
-
-
-
-
-
-
-
