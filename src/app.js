e8f3b45cb149b9307b78af73339e2606b0c8a1f2
Marlena Nikosey <mknikosey@yahoo.com>
2020-09-12 14:35:33 -0400

    Added inner.HTML changes for city, temp,  and weather description

--- src/app.js
+++ src/app.js
@@ -1,17 +1,23 @@
-function displayTemperature () {
+function displayTemperature(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.name;
+    let descriptionElement = document.querySelector("#description");
+    descriptionElement.innerHTML = response.data.weather[0].description;
+
 
 }
 
 let apiKey = "613c0e173ab2e750c5de3e90ad00ac92";
 let apiUnits = "imperial";
 let city = "Boston";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+let units = "imperial";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 
 
 axios.get(apiUrl).then(displayTemperature);
 
-
-console.log(apiUrl);
 
 
 
