27d0179694edca6ea09e80d78f745ef65a333f9d
Marlena Nikosey <mknikosey@yahoo.com>
2020-09-13 10:07:41 -0400

    Cleaned up code formatting

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ function formatDate (timestamp) {
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     let day = days[date.getDay()];
     return `${day} ${formatHours(timestamp)}`;
-
 }
 
  function formatHours(timestamp) {
